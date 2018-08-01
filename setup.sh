450f436eeed134f18d54519527dea8587966be90
Jay Smith <jaysm17h@gmail.com>
2018-08-01 22:55:02 +0000

    Fix error in setup.sh.

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ else
     real_user=$(whoami)
 fi
 
+sudo -u $real_user chmod +x aslr_ctl.sh
 sudo -u $real_user chmod +x gcc_unsafe
 
 cd /home/"${real_user}"
@@ -27,7 +28,6 @@ python setup.py install
 cd /home/"${real_user}"
 sudo -u $real_user git clone https://github.com/alset0326/peda-arm.git
 sudo -u $real_user echo "source /home/"${real_user}"/peda-arm/peda-arm.py" >> /home/"${real_user}"/.gdbinit
-sudo -u $real_user chmod +x aslr_ctl.sh
 
 echo "This script will disable ASLR systemwide until it is manually re-enabled"
 echo "or until the machine restarts. The shell script 'aslr_ctl.sh' can be used"
