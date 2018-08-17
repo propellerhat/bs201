8bebd6f8bebdced36b4f02672f06204c2f855f27
Jay <jaysmith17h@gmail.com>
2018-08-17 18:02:44 +0000

    Configure env to use pwndbg instead of peda.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# This bizzar thing below makes sure we are in the script's directory.
+# Text below is from https://stackoverflow.com/questions/6393551/what-is-the-meaning-of-0-in-a-bash-script
+#
+#It is called Parameter Expansion. Take a look at this page and the rest of the site.
+#What ${0%/*} does is, it expands the value contained within the argument 0 (which is the path that called the script) after removing the string /* suffix from the end of it.
+#So, $0 is the same as ${0} which is like any other argument, eg. $1 which you can write as ${1}. As I said $0 is special, as it's not a real argument, it's always there and represents name of script. Parameter Expansion works within the { } -- curly braces, and % is one type of Parameter Expansion.
+#%/* matches the last occurrence of / and removes anything (* means anything) after that character.
+cd "${0%/*}"
+
 if ! [ $(id -u) = 0 ]; then
    echo "This script must be run with root privs. e.g. 'sudo ${0}'" >&2
    exit 1
@@ -15,19 +24,10 @@ sudo -u $real_user chmod +x aslr_ctl.sh
 sudo -u $real_user chmod +x gcc_unsafe
 
 cd /home/"${real_user}"
-sudo -u $real_user git clone https://github.com/aquynh/capstone.git
-cd capstone/
-sudo -u $real_user ./make.sh
-./make.sh install
-cd bindings/python/
-make install
+sudo -u $real_user git clone https://github.com/pwndbg/pwndbg
+cd pwndbg/
+sudo -u $real_user ./setup.sh
 cd /home/"${real_user}"
-sudo -u $real_user git clone https://github.com/JonathanSalwan/ROPgadget.git
-cd ROPgadget/
-python setup.py install
-cd /home/"${real_user}"
-sudo -u $real_user git clone https://github.com/alset0326/peda-arm.git
-sudo -u $real_user echo "source /home/"${real_user}"/peda-arm/peda-arm.py" >> /home/"${real_user}"/.gdbinit
 
 echo "This script will disable ASLR systemwide until it is manually re-enabled"
 echo "or until the machine restarts. The shell script 'aslr_ctl.sh' can be used"
